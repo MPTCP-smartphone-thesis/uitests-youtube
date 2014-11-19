27096b137f8f0173d70f6e0ac95613c9364cbb91
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-11-19 11:18:41 +0100

    [IMP] Rewritten code

--- src/youtube/LaunchSettings.java
+++ src/youtube/LaunchSettings.java
@@ -17,92 +17,64 @@ public class LaunchSettings extends UiAutomatorTestCase {
 	private static String ID_YOUT_PLAYER = "com.google.android.youtube:id/player_fragment";
 	private static String ID_INIT_OK = "com.google.android.youtube:id/ok";
 
-	private void seeAllPopular() {
+	private static int MAX_VIDEOS = 10;
+	private static int TIME_VIDEO = 30000;
+
+	private void goToTheChannel() {
 		sleep(1500);
-		// Return to main menu  if needed
+		// Return to main menu if needed
 		while (!Utils.hasText(ID_YOUT_TITLE, "What to Watch")) {
 			getUiDevice().pressBack();
 			sleep(1500);
 		}
-		
+
 		Utils.click(ID_YOUT_SEARCH);
 
 		sleep(1000);
-		
+
 		Utils.setText(Utils.getObjectWithId(ID_YOUT_TEXT),
 				"Stanford openflow channel");
 
 		sleep(1000);
+		Utils.launchTcpdump("dailymotion", 900);
 
 		getUiDevice().pressEnter();
 
 		sleep(1000);
-		
-		Utils.click(new UiObject(
-				new UiSelector().resourceId(ID_YOUT_LIST).childSelector(
+
+		Utils.click(new UiObject(new UiSelector().resourceId(ID_YOUT_LIST)
+				.childSelector(
 						new UiSelector()
-						.className("android.widget.FrameLayout")
+								.className("android.widget.FrameLayout")
 								.instance(0))));
+	}
 
-		//
-		// Utils.click(Utils.getObjectWithDescription("What to Watch, Open guide"));
-		//
-		// sleep(1000);
-		//
-		// Utils.click(Utils.getObjectWithClassName(
-		// "android.widget.RelativeLayout", 8));
-		//
-		// sleep(1000);
-
-		// Click on first button (Popular on YouTube)
-		// UiObject popular = new UiObject(new UiSelector().resourceId(
-		// ID_YOUT_LIST).childSelector(
-		// new UiSelector().className("android.widget.FrameLayout")
-		// .instance(0)));
-		//
-		// UiScrollable mainList = Utils.getScrollableWithId(ID_YOUT_LIST);
-		//
-		// while (!Utils.click(popular)) {
-		// Utils.scrollForward(mainList);
-		// sleep(1000);
-		// popular = new UiObject(new UiSelector().resourceId(ID_YOUT_LIST)
-		// .childSelector(
-		// new UiSelector().className(
-		// "android.widget.FrameLayout").instance(0)));
-		// }
-		//
-		// sleep(1000);
-
-		// Click on Most Popular Right Now
-		// UiObject most_popular = Utils.getObjectWithClassName(
-		// "android.widget.FrameLayout", 0);
-		// assertTrue("Button most popular right now is not here",
-		// Utils.click(most_popular));
-
+	private void seeVideos() {
 		Utils.click(Utils.getObjectWithText("Videos"));
 
 		sleep(1000);
 
 		UiScrollable list = Utils.getScrollableWithId(ID_YOUT_LIST);
 		boolean canScroll = true;
-		
+		int nVid = 0;
+
 		// Goes on until no more video
-		for (int k = 0; k < 3 && canScroll; k++) {
+		for (int k = 0; k < 3 && nVid < MAX_VIDEOS && canScroll; k++) {
 			// Take the list to count the number of items in it
 			int nb_childs = Utils.getChildCount(list);
 
 			sleep(1500);
 
 			// -4 to avoid the fuzzy behavior with the video player fragment
-			for (int i = 0; i < 2 * nb_childs - 4; i = i + 2) {
+			for (int i = 0; i < 2 * nb_childs - 4 && nVid < MAX_VIDEOS; i = i + 2) {
 				UiObject video = new UiObject(new UiSelector().resourceId(
 						ID_YOUT_LIST).childSelector(
 						new UiSelector()
 								.className("android.widget.FrameLayout")
 								.instance(i)));
 				assertTrue("Reference a non-existing video", Utils.click(video));
-				sleep(30000);
-
+				sleep(TIME_VIDEO);
+				nVid++;
 				// Got it button that we can have sometimes
 				if (Utils.hasObject(ID_INIT_OK))
 					Utils.click(ID_INIT_OK);
@@ -116,7 +88,6 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		}
 	}
 
-
 	public void testDemo() throws UiObjectNotFoundException {
 		assertTrue("OOOOOpps",
 				Utils.openApp(this, "YouTube", "com.google.android.youtube"));
@@ -127,8 +98,8 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		// initial menu: just clic on OK
 		if (Utils.hasObject(ID_INIT_OK))
 			Utils.click(ID_INIT_OK);
-
-		seeAllPopular();
+		goToTheChannel();
+		seeVideos();
 		Utils.killTcpdump();
 	}
 
